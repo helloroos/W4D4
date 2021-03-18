1da68d9c0b02546b13b3d986be5cddd93b115cb1
juliajykim <jkim24@gwu.edu>
2021-03-18 14:04:19 -0400

    Add #my_uniq Rspec

--- Rspec/spec/tdd_project_spec.rb
+++ Rspec/spec/tdd_project_spec.rb
@@ -1,1 +1,14 @@
 require "tdd_project"
+
+describe "tdd" do 
+
+  describe "#my_uniq" do
+    arr = [1,2,1,3,3]
+    it "takes in an array" do
+      expect(my_uniq(arr)).to eq([1,2,3])
+    end
+  end
+
+end
+
+
